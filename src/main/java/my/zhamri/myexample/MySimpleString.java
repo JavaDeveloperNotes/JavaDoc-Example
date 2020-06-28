9aa577d6dd8f29a541dd2e0f46eef305349d13da
Zhamri Che Ani <zhamri@gmail.com>
2020-06-28 15:41:48 +0800

    Initial Commit

--- src/main/java/my/zhamri/myexample/MySimpleString.java
+++ src/main/java/my/zhamri/myexample/MySimpleString.java
@@ -1,4 +1,30 @@
 package my.zhamri.myexample;
 
+/**
+ * This class is for manipulating strings
+ *
+ * @author Zhamri Che Ani
+ */
 public class MySimpleString {
+
+    /**
+     * This method is for counting the length of your name and your home address.
+     *
+     * @param name Your full name which including the first name and last name.
+     * @param address Your home address.
+     * @return The total length of the name and address.
+     */
+    public int getLength(String name, String address){
+        int length = name.length() + address.length();
+        return length;
+    }
+
+    /**
+     * This method is for displaying a string.
+     *
+     * @param str Either your name or your address.
+     */
+    public void displayString(String str){
+        System.out.println("The string is " + str);
+    }
 }
--- src/main/java/my/zhamri/myexample/Test.java
+++ src/main/java/my/zhamri/myexample/Test.java
@@ -1,4 +1,16 @@
 package my.zhamri.myexample;
 
-public class App {
+public class Test {
+
+    public static void main(String[] args) {
+
+        String name = "Zhamri Che Ani";
+        String address = "Kuala Lumpur, Malaysia";
+
+        MySimpleString mySimpleString = new MySimpleString();
+        mySimpleString.displayString(name);
+        mySimpleString.displayString(address);
+        System.out.println("Total length: " + mySimpleString.getLength(name,address));
+
+    }
 }
